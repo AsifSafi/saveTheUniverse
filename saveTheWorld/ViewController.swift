cd9d7fd07dfdb2a81bdec26f7dc19b9db0cacd38
Muhammad Asif <masif.safi@hotmail.com>
2017-06-30 19:07:22 +0200

    Initial Commit

---
+++ saveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saveTheWorld
+//
+//  Created by Muhammad Asif on 6/30/17.
+//  Copyright © 2017 Muhammad Asif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
